1395fafbf0d055b385a2e71b31ac40ab4e4aa6c7
Cvetomir M. Dimov <cvetomir.dimov@unige.ch>
2026-05-26 19:17:57 +0200

    Added github actions to setup automatic deployment

--- build.clj
+++ build.clj
@@ -3,7 +3,7 @@
             [deps-deploy.deps-deploy :as dd]))
 
 (def lib 'com.github.cmd-mov/clj-elan)
-(def version "0.2.1")
+(def version (or (System/getenv "PROJECT_VERSION") "0.2.1-SNAPSHOT"))
 (def class-dir "target/classes")
 (def basis (b/create-basis {:project "deps.edn"}))
 (def jar-file (format "target/%s-%s.jar" (name lib) version))
@@ -16,16 +16,14 @@
                 :lib lib
                 :version version
                 :basis basis
-                :src-dirs ["src"]
-                #_:pom-data #_[[:licenses
-                            [:license {:name "Eclipse Public License 2.0"
-                                       :url "https://www.eclipse.org/legal/epl-2.0/"}]]]})
+                :src-dirs ["src"]})
   (b/copy-dir {:src-dirs ["src" "resources"]
                :target-dir class-dir})
   (b/jar {:class-dir class-dir
           :jar-file jar-file}))
 
-(defn deploy [_]
-  (dd/deploy {:installer :remote
-              :artifact jar-file
-              :pom-file (b/pom-path {:lib lib :class-dir class-dir})}))
+(defn deploy [opts]
+  (let [{:keys [class-dir jar-file]} (jar opts)]
+    (dd/deploy {:installer :remote
+                :artifact (b/resolve-path jar-file)
+                :pom-file (b/pom-path {:lib lib :class-dir class-dir})})))
